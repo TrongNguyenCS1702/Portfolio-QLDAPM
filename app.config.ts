f0243cd85189d3e9b33477a0c06108129219423a
Tyson Nguyen <105373326+TrongNguyenCS1702@users.noreply.github.com>
2024-04-14 15:58:10 +0700

    Add Home, Structure for Projects, About, etc....

---
+++ app.config.ts
@@ -0,0 +1,9 @@
+export default defineAppConfig({
+  // Configure Content Wind here
+  cover: '/cover.jpg',
+  socials: {
+    twitter: 'NguyenV47261822',
+    github: 'TrongNguyenCS1702',
+    linkedin: 'tysonnguyensec'
+  }
+})
