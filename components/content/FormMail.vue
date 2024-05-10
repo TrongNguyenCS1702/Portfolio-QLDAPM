626543db8dd805c3b3a305d10b5c3c0cd092ccc0
Tyson Nguyen <105373326+TrongNguyenCS1702@users.noreply.github.com>
2024-05-10 16:37:21 +0700

    fix SEO critical

--- components/content/FormMail.vue
+++ components/content/FormMail.vue
@@ -2,7 +2,7 @@
 <template>
     <div class="flex flex-col items-center justify-center min-h-screen py-2">
       <div class="flex flex-col w-full p-4 md:w-1/2 lg:w-1/2">
-        <h1 class="mb-4 text-3xl font-bold text-center">Contact Me</h1>
+        <h2 class="mb-4 text-3xl font-bold text-center">Contact Me</h2>
         <form @submit.prevent="submitForm" class="flex flex-col">
           <input v-model="name" type="text" placeholder="Your Name" required class="p-2 mb-2 border rounded" />
           <input v-model="email" type="email" placeholder="Your Email" required class="p-2 mb-2 border rounded" />
@@ -10,7 +10,7 @@
           <button type="submit" class="p-2 mt-4 text-white bg-blue-500 rounded hover:bg-blue-700">Send</button>
         </form>
         <div class="mt-4 text-center">
-          <h2 class="mb-2 text-lg font-bold">Or connect with me on:</h2>
+          <h3 class="mb-2 text-lg font-bold">Or connect with me on:</h3>
           <a href="mailto:trong.nguyentech1702@gmail.com" class="block mb-1 text-blue-500 hover:underline">Gmail</a>
           <a href="https://www.linkedin.com/in/tysonnguyensec" class="block text-blue-500 hover:underline">LinkedIn</a>
         </div>
